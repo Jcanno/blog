74ab2d31621415859c6ac893c507826aa46f14e4
Jacano <641283231@qq.com>
2020-08-22 08:42:02 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  title: '前端体系',
+	description: '构建前端体系',
+	themeConfig: {
+    repo: 'Jcanno/blog',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+		sidebar: [
+      {
+        title: 'JS基础',
+        collapsable: false,
+        children: [
+          '/js/scope',
+        ]
+			},
+			{
+        title: 'CSS',
+        collapsable: false,
+        children: [
+        ]
+			},
+			{
+        title: '工程化',
+        collapsable: false,
+        children: [
+        ]
+			},
+			{
+        title: '性能优化',
+        collapsable: false,
+        children: [
+        ]
+			},
+			{
+        title: '网络协议',
+        collapsable: false,
+        children: [
+        ]
+			},
+			{
+        title: '浏览器原理',
+        collapsable: false,
+        children: [
+        ]
+      },
+		]
+	}
+}
